d746501256eab62697861158b0377267dc5d87e1
Abel Zazueta <abzazuet@amazon.com>
2022-09-29 08:44:07 -0700

    Updated serializer to include preescriptions

--- app/serializers/appointment_serializer.rb
+++ app/serializers/appointment_serializer.rb
@@ -2,4 +2,5 @@ class AppointmentSerializer < ActiveModel::Serializer
   attributes :id, :doctor_id, :patient_id, :day, :motive, :status
   belongs_to :doctor
   belongs_to :patient
+  has_many :preescriptions, dependent: :destroy
 end
